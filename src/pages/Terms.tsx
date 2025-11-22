b0fbdfdf7e9ae0c4be85ba8b860cd1816082dae0
kic0 <49309708+kic0@users.noreply.github.com>
2025-11-22 17:09:58 +0000

    Update Terms and Conditions text and links

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -7,18 +7,19 @@ const Terms = () => {
       <main className="container max-w-3xl">
         <Card className="bg-card">
           <CardHeader>
-            <CardTitle className="text-3xl">Termos de Participação</CardTitle>
+            <CardTitle className="text-3xl">Termos e condições de participação</CardTitle>
           </CardHeader>
           <CardContent className="space-y-4 text-lg">
-            <p>• Seus depoimentos serão gravados anonimamente.</p>
-            <p>• Podem ser usados em atividades educativas e culturais.</p>
-            <p>• Você pode desistir a qualquer momento.</p>
+            <p>• Os meus depoimentos serão registados anonimamente.</p>
+            <p>• A informação recolhida pode ser usada em atividades educativas e culturais.</p>
+            <p>• Poderei desistir a qualquer momento.</p>
+            <p>Mais informação sobre o âmbito do projeto em futuroemrede.com</p>
             <div className="flex gap-4 pt-4">
               <Button variant="hero" size="lg" asChild>
-                <a href="/demographics/genero">ACEITO</a>
+                <a href="/demographics/genero">Aceito</a>
               </Button>
               <Button variant="outline" size="lg" asChild>
-                <a href="/">NÃO ACEITO</a>
+                <a href="/">Voltar</a>
               </Button>
             </div>
           </CardContent>
